8bd6bff92a28f68e66fc28d55daf9c2a622e900d
Chris Wright <chriswrightdev@gmail.com>
2022-07-26 21:56:51 +0100

    get news data working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import useBasic from "../data/queryHooks/useBasic";
-
+import useNews from "../data/queryHooks/useNews";
 function Home() {
   const basicFestivalData = useBasic();
-  if (basicFestivalData.isLoading) {
+  const allNewsData = useNews();
+  // gets all the news -  cap at 20 on the api
+  if (basicFestivalData.isLoading || allNewsData.isLoading) {
     return null;
     // maybe add placeholder component with text to read until data is loaded
   } else {
     const festData = basicFestivalData.data[0]; //only 1 row
+    const newsData = allNewsData.data;
     console.log("festData", festData);
+    console.log("newsData", newsData);
     return <div>Home</div>;
   }
 }
