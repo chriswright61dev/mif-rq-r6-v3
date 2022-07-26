b4f1d1fe884e8e590c8c57312ead51a9b70ecc3c
Chris Wright <chriswrightdev@gmail.com>
2022-07-26 21:47:16 +0100

    home page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+import Home from "./pages/Home";
 function App() {
-  return <div className="App">hi</div>;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
