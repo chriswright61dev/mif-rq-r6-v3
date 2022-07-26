79792b62409697942b08f9f2c7c652706692c383
Chris Wright <chriswrightdev@gmail.com>
2022-07-26 21:51:05 +0100

    fetch some data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
+import useBasic from "../data/queryHooks/useBasic";
+
 function Home() {
-  return <div>Home</div>;
+  const basicFestivalData = useBasic();
+  if (basicFestivalData.isLoading) {
+    return null;
+    // maybe add placeholder component with text to read until data is loaded
+  } else {
+    const festData = basicFestivalData.data[0]; //only 1 row
+    console.log("festData", festData);
+    return <div>Home</div>;
+  }
 }
 
 export default Home;
